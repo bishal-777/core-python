f1d2d4641db1baad2a7ff9883e14d90b917aeadb
bishal <bishal777giri@gmail.com>
2026-03-05 23:31:08 +0545

    done bitwise operator

---
+++ operators/05-bitwise.py
@@ -0,0 +1,9 @@
+# & --> if both bits 1 result 1 else 0
+# | --> if atleast one bit 1 result 1 else 0
+# ^ --> if both bits are different result 1 else 0
+# ~ --> if  bit 1n result 1 and vice versa
+print(4&5)  #4=100, 5=101 (binary) 
+print(4|5)
+print(4^5)
+print(~5)
+print(~-4)
