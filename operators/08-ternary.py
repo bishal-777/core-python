7b0aecd80c6c7ad1fcbbcd7934f59f88b45a4017
bishal <bishal777giri@gmail.com>
2026-03-06 23:56:43 +0545

    more operators

---
+++ operators/08-ternary.py
@@ -0,0 +1,11 @@
+#also called conditional operator
+
+a = 10
+b = 20
+x = 1 if a>b else 0
+print(x)
+
+a = int(input("Enter first number "))
+b = int(input("Enter secomd number "))
+min = a if a<b else b
+print(min)
